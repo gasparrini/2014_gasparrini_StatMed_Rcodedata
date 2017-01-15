185897734597926238bf472e05639ac7621d27ea
Antonio Gasparrini <gaspanero@hotmail.com>
2017-01-15 15:24:30 +0000

    Check update to dlnm version 2.3.2

--- simprep.R
+++ simprep.R
@@ -5,13 +5,12 @@
 #     non-linear models"
 #   Antonio Gasparrini
 #   Statistics in Medicine - 2014
-#   http://www.ag-myresearch.com/statmed2014.html
+#   http://www.ag-myresearch.com/2014_gasparrini_statmed.html
 #
-# Update: 14 December 2016
-# For any problem with this code, please contact antonio.gasparrini@lshtm.ac.uk
-# Please refer to the original code for any copyright issue
-#
-#  See www.ag-myresearch.com for future updates
+# Update: 15 January 2017
+# * an updated version of this code, compatible with future versions of the
+#   software, is available at:
+#   https://github.com/gasparrini/2014_gasparrini_StatMed_Rcodedata
 ################################################################################
 
 ################################################################################
